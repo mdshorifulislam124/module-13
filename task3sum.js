0fa4585a3146671c7153c81c243b3bf2d359d342
mdshorifulislam124 <asunhadispori11@gmail.com>
2026-05-02 13:55:12 +0600

    odd number

--- task3sum.js
+++ task3sum.js
@@ -1,11 +1,25 @@
-let num = 81;
+// let num = 81;
+// let sum = 0;
+
+// while (num <= 131) {
+//     if (num % 2 === 0) {   //  even check
+//         sum += num;        //  তখনই যোগ
+//     }
+//     num++;
+// }
+
+// console.log("Total Even Sum:", sum);
+
+
+
+let num = 206;
 let sum = 0;
 
-while (num <= 131) {
-    if (num % 2 === 0) {   //  even check
+while (num <= 311) {
+    if (num % 2 !== 0) {   //  Odd check
         sum += num;        //  তখনই যোগ
     }
     num++;
 }
 
-console.log("Total Even Sum:", sum);
+console.log("Total Odd Sum:", sum);

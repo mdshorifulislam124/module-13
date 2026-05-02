a52a23a35815724aaf8e5341cbd11a3d76840498
mdshorifulislam124 <asunhadispori11@gmail.com>
2026-05-02 12:37:33 +0600

    while loop

---
+++ while.js
@@ -0,0 +1,14 @@
+// let num = 0;
+// while(num < 5){
+//     console.log('I Love You')
+// }
+// amon ta kora jabe na ata akta infinite loop choltei thakbe
+
+let num = 0;
+while (num < 5){
+    console.log('I Love You')
+    // num = num + 1;
+    // num =+ 1;  atao infinite loop
+    // num += 1;
+    num++;
+}

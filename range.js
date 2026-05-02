52a95c4beba1ca997ce98184b38869b6671113f7
mdshorifulislam124 <asunhadispori11@gmail.com>
2026-05-02 13:12:42 +0600

    range for even

---
+++ range.js
@@ -0,0 +1,15 @@
+// let num = 1;
+// while(num <= 10){
+//     console.log(num);
+//     num++;
+// }
+
+let num = 1;
+while(num <= 10){
+    console.log(num);
+
+    if(num % 2 === 0){
+        console.log('Even number:', num);
+    }
+    num++;
+}

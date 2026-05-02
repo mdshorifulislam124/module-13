a53d67c1c4e2c8016a85f2bc4094999fa20815de
mdshorifulislam124 <asunhadispori11@gmail.com>
2026-05-02 14:26:53 +0600

    boro thake soto

--- multi.js
+++ multi.js
@@ -1,7 +1,15 @@
-let num = 5;
-let i = 1;
+// let num = 5;
+// let i = 1;
 
-while (i <= 10) {
-    console.log(num + " x " + i + " = " + (num * i));
-    i++;
+// while (i <= 10) {
+//     console.log(num + " x " + i + " = " + (num * i));
+//     i++;
+// }
+
+
+let num = 21;
+
+while (num >= 15) {
+    console.log(num);
+    num--;   // প্রতি বার ১ করে কমবে
 }

c7ad1d44ca19d6f7d853401cdeef44886a2bf803
mdshorifulislam124 <asunhadispori11@gmail.com>
2026-05-02 13:33:07 +0600

    odd number

--- task2W.js
+++ task2W.js
@@ -1,9 +1,19 @@
-let num = 61
+// let num = 61
 
-while(num <= 100){
+// while(num <= 100){
+//     console.log(num);
+//     if(num % 2 === 0){
+//         console.log('Even', num)
+//     }
+//     num++;
+// }
+
+let num = 78
+
+while(num <= 98){
     console.log(num);
-    if(num % 2 === 0){
-        console.log('Even', num)
+    if(num % 2 !== 0){
+        console.log('Odd', num)
     }
     num++;
 }

4fec6d6963f3766b6c1470ff1910c114cd760305
mdshorifulislam124 <asunhadispori11@gmail.com>
2026-05-02 12:08:02 +0600

    module13 class 1 for loop

---
+++ forloop.js
@@ -0,0 +1,19 @@
+/*
+
+Array = [3, 4, 5, 3, 5]
+
+console.log(Array);
+
+for(const num of Array){
+    console.log(num);
+}
+
+*/
+
+const fol = ['am', 'jam', 'kola', 'pape']
+
+console.log(fol);
+for(fols of fol){
+    console.log('I want to eat:')
+    console.log(fols);
+}

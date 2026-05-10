1f0513b2c361e6132b54243375b381ce4559c311
mdshorifulislam124 <asunhadispori11@gmail.com>
2026-05-10 08:23:39 +0600

    even number sum

--- for3.js
+++ for3.js
@@ -1,11 +1,18 @@
-for odd number sum
+// for odd number sum
 
+// let sum = 0;
+// for(let i = 91; i <= 129; i += 2){
+//     console.log(i)
+//     sum = sum + i
+
+// }
+// console.log('odd numbers from 91 to 129 is:', sum)
+
+
+// for even number sum
 let sum = 0;
-for(let i = 91; i <= 129; i += 2){
+for(let i =50; i <=85; i +=2 ){
     console.log(i)
     sum = sum + i
-
 }
-console.log('odd numbers from 91 to 129 is:', sum)
-
-
+console.log('evan num from 51 to 85 sum is:', sum)
